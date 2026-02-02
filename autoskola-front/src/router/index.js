b5e93cdee3e8e2771e72588a2b325b2147031342
alim344 <milabudimirovic2@gmail.com>
2026-02-02 20:56:18 +0100

    Adding login logic

--- autoskola-front/src/router/index.js
+++ autoskola-front/src/router/index.js
@@ -4,11 +4,21 @@ import { createRouter, createWebHistory } from 'vue-router';
 //import HelloWorld from '@/components/HelloWorld.vue'; // Update path if necessary
 import HomeView from '@/view/HomeView/HomeView.vue'
 import RegistrationView from '@/view/RegistrationView.vue';
+import LoginView from '@/view/LoginView.vue';
+import CandidateHomeView from '@/view/HomeView/CandidateHomeView.vue';
+import InstructorHomeView from '@/view/HomeView/InstructorHomeView.vue';
+import ProfessorHomeView from '@/view/HomeView/ProfessorHomeView.vue';
+import AdminHomeView from '@/view/HomeView/AdminHomeView.vue';
 
 
 const routes = [
   { path: '/', component: HomeView },
   {path: '/register', component: RegistrationView},
+  {path:'/login', component: LoginView},
+  {path:'/candidateHome', component: CandidateHomeView},
+  {path:'/instructorHome', component:InstructorHomeView},
+  {path:'/professorHome', component:ProfessorHomeView},
+  {path:'/adminHome', component:AdminHomeView}
  
 ];
 
--- autoskola-front/src/view/HomeView/AdminHomeView.vue
+++ autoskola-front/src/view/HomeView/AdminHomeView.vue
@@ -1,14 +1,99 @@
 <template>
-    
+    <div>
+        <header class="top"> 
+            <div class ="logo"> 
+                <span class = "big_logo">TaSaMi</span>
+                <span class = "small_logo">Car school</span>
+            </div>
+            <div class = "authbuttons"> 
+                
+                <button class = "log button" @click="navigateToHomePage">Log out</button>
+            </div>
+        </header>
+        <div class="brisiteovo">
+            <h1>ADMIN</h1>
+        </div>
+    </div>
 </template>
 
 <script>
 
 export default {
-    
+    methods:{
+        navigateToHomePage(){
+            localStorage.removeItem('token');
+            localStorage.removeItem('role');
+
+            this.$router.push('/');
+        }
+    }
 }
 </script>
 
 <style scoped>
-/*admin vidi ovu stranu kad se uloguje*/
+.top{
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  width: 100%;
+  height: 80px;
+  background-color: rgb(154, 154, 174);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+}
+
+.logo{
+  margin:20px;
+
+}
+
+.big_logo{
+  font-size: 40px;
+  font-weight: 700;
+  color: white;
+}
+
+.small_logo{
+  font-size: 19px;
+  opacity: 0.8;
+  margin-left: 5px;
+}
+
+.authbuttons{
+  margin:10px;
+  align-content: center;
+  padding: 10px;
+  justify-content: space-between;
+}
+
+.button{
+  border: 2px;
+  padding: 8px 15px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.log{
+  border-color:rgb(58, 40, 60);
+  box-shadow: 0 4px 6px rgba(39, 27, 27, 0.9);
+  font-size: 20px;
+  background-color: rgb(190, 143, 233);
+  transition: transform 0.8s cubic-bezier(0.34, 1.56, 0.64, 1);
+}
+
+
+.log:hover{
+   color: #484264;
+  background-color: #342b56;
+  color:#fef9fb;
+  transform: scale(1.1);
+  box-shadow: 0 0 20px rgba(199, 147, 221, 0.8); /* Blue glow */
+}
+
+.brisiteovo{
+    margin-top: 150px;
+}
 </style>
